31f8e7c35488a96fdc74e33cab2971fbcce68dd5
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-23 12:21:47 -0700

    type of ds

--- notes.py
+++ notes.py
@@ -11,3 +11,30 @@ and also both of them are not efficient when it comes to to search in item in li
 so we are gonna use the search binary to find an item
 '''
 
+
+                                         # The type of data structure
+'''
+There 2 types of it 
+   A- Primitive Data Structure      B- Non-Primitive Data Structure
+
+                  A- Primitive Data Structure 
+
+    1. Characters    2.Float     3.Integer   4. Pointer   
+-----------------------------------------------------------------------
+
+
+                       B-Non-Primitive Data Structure
+        1.Liner data structure                        2. Non_liner data structure
+--------------------------------------------------------------------------
+
+    
+           1.Liner data structure 
+           a.Arrays     b.Liked list    c.Stack     d.Queue
+           ------------------------------------------------
+
+           2.Non_liner data structure
+           a.Graphs   b.Trees
+'''
+
+
+ 
