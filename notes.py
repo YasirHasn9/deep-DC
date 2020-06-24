435c7b5008db101c9865c84e1dbaa708c90ec126
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-23 20:39:06 -0700

    time comlexity

--- notes.py
+++ notes.py
@@ -62,3 +62,72 @@ or the number could be in the middle of the list and this case called an average
 so now we have 3 cases for the time
 1.Best Case  Omega              2.Average Case  Theta         3. Worst Case(most important)  Big O Notaion(Order)
 '''
+
+
+                             # Complexcity  
+'''
+Our mission as programmers is to make the worst case be best case or at least make an average 
+but to make this mission easier we need to know the Complexcity of the operation 
+------------------------------------------------------------------------------------
+for instance if we want to loop over a numbers to get the sum of them
+'''
+sum = 0
+for i in range(0,6):
+    sum += i
+# print(sum)
+
+'''
+so now we have to get every number between the 0 and 6 to add them up to the sum
+for i in range(0,6): --------> this line is O(n)
+    sum += i ----------> is O(1) Constant Time which is gonna happend just once
+
+now for-loop(iterations) is a big O(n)
+because of this iterations we refere to this operation as O(n)
+
+
+Time Complexity == O(n) + O(1) =====> O(n)
+-----------------------------
+'''
+
+'''
+lets say you have 
+for i in something:
+    # this is the outer loop
+    // do something
+    for j in in somethinElse:
+        # this is the inner loop
+        // do something else
+
+in this case it is O(n^2)
+'''
+
+
+
+# Another one
+'''
+i,n
+for(i ; i < n ; i = i*2){
+    do something 
+}
+This is a log(n)
+since the operation inside the loop is * or /
+'''
+
+# # fibonacci 
+# def fab(n):
+#     print(n)
+#     # base case is where the program stops its work
+#     if n < 1:
+#         return n
+#     else:
+#         return fab(n / 2)
+
+# print(fab(100))
+
+# def rec(n):
+#     if n <= 0 :
+#         return 1
+#     else:
+#         return 1 + rec(n-1)
+
+# print(rec(8))
