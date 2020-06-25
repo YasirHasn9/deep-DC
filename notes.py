7cd992e6b26d62b01796bc04e9fc1a86012015c9
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-25 12:43:33 -0700

    linked list details

--- notes.py
+++ notes.py
@@ -63,8 +63,7 @@ so now we have 3 cases for the time
 1.Best Case  Omega              2.Average Case  Theta         3. Worst Case(most important)  Big O Notaion(Order)
 '''
 
-
-                             # Complexcity  
+# Complexcity
 '''
 Our mission as programmers is to make the worst case be best case or at least make an average 
 but to make this mission easier we need to know the Complexcity of the operation 
@@ -72,7 +71,7 @@ but to make this mission easier we need to know the Complexcity of the operation
 for instance if we want to loop over a numbers to get the sum of them
 '''
 sum = 0
-for i in range(0,6):
+for i in range(0, 6):
     sum += i
 # print(sum)
 
@@ -102,7 +101,6 @@ in this case it is O(n^2)
 '''
 
 
-
 # Another one
 '''
 i,n
@@ -113,7 +111,7 @@ This is a log(n)
 since the operation inside the loop is * or /
 '''
 
-# # fibonacci 
+# # fibonacci
 # def fab(n):
 #     print(n)
 #     # base case is where the program stops its work
@@ -132,8 +130,7 @@ since the operation inside the loop is * or /
 
 # print(rec(8))
 
-
-                                       #Stack
+# Stack
 '''
 
 you can think of stack as array, the first item you put in the array 
@@ -155,7 +152,7 @@ or
 LIFO
 '''
 
-                    # Array based implementation 
+# Array based implementation
 '''
 The stack has 4 main functions 
 1.push() 
@@ -170,3 +167,114 @@ is to get the last value been insterted in the stack(top)
 4.isEmpty()
 is to check if the stack is empty
 '''
+# Stack.py
+
+# Liked List
+'''
+The best thing about array is that they do have fixed access time O(1)
+features of arrays
+1.fixed access time
+2.sequential
+3.bad removal 
+4.bad insertion
+5.space westage
+
+Now lets talk about the liked list
+1.dynamic size
+2.no space westage 
+3.good removal
+4.good insertion
+4. sequential space dose not required
+5.random access not allowed
+
+SO , How does it work?
+ ---Each element in the linked list is been saved certain place in the memory
+    and is not necessary to be sequential
+
+How can i recognize them in the memory ?
+--- Each item is made of a value and a pointer , inside in the pointer would the address of the next
+item in the linked list. We are gonna call this combination A Node
+item + pointer = Node
+class Node:
+    def __init__(self, value, next)
+'''
+
+
+# class Node:
+#     def __init__(self, item):
+#         self.item = item
+#         self.next = None
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def add_to_head(self, value):
+        newNode = Node(value)
+        # if there no node in the list
+        if not self.head:
+            self.head = newNode
+            newNode.next = None
+        else:
+            # now we are just change the pointer of the nex node to old item
+            # or the item before the newNode
+            newNode.next = self.head
+            # make the head points of the first node in the list
+            self.head = newNode
+
+    def add_to_tail(self, value):
+        newNode = Node(value)
+        # first we check if the if the tail.next == none
+        # since it is the last item in the linkedlist
+        if self.tail.next is None:
+            # now we have to make the tail is the newNode 
+            # by pointing the next of the tail to the new one
+            self.tail.next = newNode
+            # sine the newNode is last one in the list so the next of the newNode 
+            # should points to None
+            newNode.next = None
+            # also we should make sure that the tail points at the 
+            # node in the list
+            self.tail = newNode
+
+
+# a = [1,2,3,4]
+# b = [1,3,5,6]
+# a.extend(b)
+# print(list(set(a)))
+
+# def insertSortArr(arr ,  n):
+#     if n == 2:
+#         return
+#     else:
+#         insertSortArr(arr,n-1)
+#         last = arr[n-1]
+#         j = n - 2
+#         while(j >= 0 and arr[j] > last):
+#             arr[j+1] = arr[j]
+#             j= j -1
+#         arr[j+1] = last
+
+# print(insertSortArr([2,5,2,6,7,3,4],5))
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def insert_first(self, value):
+        newNode = Node(value)
+        if not self.head:
+            self.head = newNode
+            newNode.next = None
+        else:
+            newNode.next = self.head
+            self.head = newNode
